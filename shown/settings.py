8300898ba4e60ff94abf039d5b74047d61584cd5
Anna Klimova <anna.klimova@renault.com>
2021-03-18 14:08:15 +0300

    allowed

--- shown/settings.py
+++ shown/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ljblw@atal*x4#!6t0je!f0)^$kmm^e4%*1vuuhju!yjn*rxu6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
