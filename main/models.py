91799be6bba559f188658369dc8039d50c0f6370
Anna Klimova <anna.klimova@renault.com>
2021-03-18 17:12:29 +0300

    test23

--- main/models.py
+++ main/models.py
@@ -14,7 +14,7 @@ class Auths(models.Model):
     EmailConfirmed=models.BooleanField()
 
     class Meta:
-        db_table='public\".\""Auths"'
+        db_table='"Auths"'
 
 
 class EmployeeOwners(models.Model):
